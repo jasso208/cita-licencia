7ad1046a193bddf2ac1156d833e57d431a764d35
jasso <jasso.gallegos@gmail.com>
2023-12-01 22:22:28 -0600

    Reenvio de codigo token

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
 import { GeneralService } from 'src/app/servicios/general.service';
 
 @Component({
@@ -11,7 +12,10 @@ export class LoginComponent {
 
   public basic:boolean;
   public show_form_envio_token:boolean;
-
+  public sppiner_div:boolean;
+  public segundos:number;
+  
+  private interval:any;
   public form:FormGroup;
   constructor(
     private fb:FormBuilder,
@@ -19,6 +23,8 @@ export class LoginComponent {
   ){
     this.basic = true;
     this.show_form_envio_token = true;
+    this.sppiner_div = false;
+    this.segundos = 60;
 
     this.form = this.fb.group({
       email:[''],
@@ -28,12 +34,50 @@ export class LoginComponent {
   }
 
   envioToken():any{
+    this.sppiner_div = true;
     this.show_form_envio_token = false;
-    this.gservice.generaToken("cliente/validaCliente",this.form.get("email")?.value,this.form.get("whatsapp")?.value)
+    this.gservice.generaToken("cliente/tokenCliente",this.form.get("email")?.value,this.form.get("whatsapp")?.value)
     .subscribe(
       data=>{
         console.log(data);
+        this.sppiner_div = false;
+        this.segundos = 10;
+        this.enviacodigo();
+       
+      },
+      error => {
+        this.sppiner_div = false;
+        this.enviacodigo();
       }
     );
   }
+
+  enviacodigo(){
+    this.interval =  setInterval(
+      ()=>{
+        this.timmer();
+      },
+      1000
+    );
+  }
+  timmer():any{
+    
+    console.log(this.segundos);
+    this.segundos = this.segundos - 1;
+    
+    if(this.segundos == 0){
+      console.log("muestra boton");    
+      clearInterval(this.interval);  
+      
+    }
+   
+
+  }
+
+  cambiarWhatsappEmail():any{
+    this.show_form_envio_token = true;
+  }
+
+
+
 }
