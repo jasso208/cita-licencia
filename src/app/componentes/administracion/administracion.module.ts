7785b3e5a5f61f9444d1ca463443f8bc20bfd991
jasso <jasso.gallegos@gmail.com>
2023-12-07 15:20:05 -0600

    se crea componente administración

---
+++ src/app/componentes/administracion/administracion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdministracionComponent } from './administracion.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdministracionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdministracionModule { }
